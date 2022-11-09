84b610bda2cb462f6b00723ccc025523eff12924
ljy <link.twilight.gu@gmail.com>
2022-11-09 16:02:14 +0800

    传回一个页面

--- pro01-fruit/stage03-servlet/src/main/java/com/learn/servlets/AddServlet.java
+++ pro01-fruit/stage03-servlet/src/main/java/com/learn/servlets/AddServlet.java
@@ -9,12 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
 
     FruitDao fruitDao = new FruitDaoImpl();
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setCharacterEncoding("UTF-8");
+
         Fruit fruit = new Fruit();
         fruit.setFname(req.getParameter("fname"));
         fruit.setPrice(Integer.parseInt(req.getParameter("price")));
@@ -23,6 +27,11 @@ public class AddServlet extends HttpServlet {
 
         fruitDao.addFruit(fruit);
 
-        System.out.println("添加成功");
+//        System.out.println("添加成功");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.print("<p>添加成功</p>");
+        writer.flush();
     }
 }
